28898c1728833cfa7e254d75c37b7d271ba91ca3
Violet Stewart <vkazoo2@gmail.com>
2021-09-29 13:08:49 -0700

    made nametag better

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// import functions and grab DOM elements
+const nameInput = document.getElementById('name');
+const button = document.getElementById('generate');
+const firstName = document.getElementById('first-name');
 
-// initialize global state
+button.addEventListener('click', ()=>{
+    //console.log('I am clicking');
+    let newName = nameInput.value;
+    firstName.textContent = newName;
+});
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
