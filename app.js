976b4217970b368125f0cf15bf0aec37b392766c
Violet Stewart <vkazoo2@gmail.com>
2021-09-29 13:51:41 -0700

    pink and pronouns added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const nameInput = document.getElementById('name');
 const button = document.getElementById('generate');
 const firstName = document.getElementById('first-name');
+const pinkButton = document.getElementById('pink');
+const pronounInput = document.getElementById('pronouns');
+const pronounButton = document.getElementById('pronoun-button');
+const newPronounInput = document.getElementById('my-pronouns')
+//const buttonTwo = document.body.style.backgroundColor
+//document.body.style.backgroundColor('pink');
 
 button.addEventListener('click', ()=>{
     //console.log('I am clicking');
@@ -8,3 +14,13 @@ button.addEventListener('click', ()=>{
     firstName.textContent = newName;
 });
 
+pronounButton.addEventListener('click', ()=>{
+  let newPronouns = pronounInput.value;
+  newPronounInput.textContent = newPronouns;
+});
+
+pinkButton.addEventListener('click', ()=>{
+    console.log('click');
+    document.body.style.color = 'hotpink';
+
+});
